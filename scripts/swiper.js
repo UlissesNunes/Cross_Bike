ce26fa3c05673461a4d1b532b2f95bd219930e70
UlissesNunes <ulissesnunesx@gmail.com>
2025-04-11 12:37:53 -0300

    atualização

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -20,7 +20,39 @@
       0: {
         slidesPerView: 1,
       } ,
-      767 : {
+      667 : {
+        slidesPerView: 2,       
+      },
+      1024 : {
+        slidesPerView: 4,
+      },
+
+    }
+  });
+
+
+  const swiperOutro = new Swiper('.produtos-CB', { 
+    loop: true,
+    spaceBetween: 30,
+    autoplay: { delay: 4000 },
+    
+    pagination: {
+      el: '.swiper-pagination',
+      clickable : true ,
+      dynamicBullets: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      } ,
+      667 : {
         slidesPerView: 2,       
       },
       1024 : {
