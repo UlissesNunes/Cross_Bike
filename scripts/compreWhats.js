53e37466bcd6065afd271edba266dd809ecea8c3
UlissesNunes <ulissesnunesx@gmail.com>
2025-08-13 20:45:40 -0300

    updatee

--- scripts/compreWhats.js
+++ scripts/compreWhats.js
@@ -2,4 +2,4 @@
     const telefone = '5571987261711'; // seu número com DDD
     const mensagem = `Olá! Vim pelo Site Oficial da CrossBike Tenho interesse no produto: ${produto}.  Poderia me enviar mais informações?`;
     const url = `https://wa.me/${telefone}?text=${encodeURIComponent(mensagem)}`;
-    window.open(url, '_blank'); }
+    window.open(url, '_blank'); }     
